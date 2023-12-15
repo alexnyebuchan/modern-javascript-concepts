ceb06e4df82dfe5dfd0c3728cfecc99f6166ed7d
alexnyebuchan <alexnyebuchan@gmail.com>
2023-12-15 15:58:50 +0000

    Added pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import styles from './styles/App.module.scss'
 import Sidebar from "./components/Sidebar"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import HomePage from "./components/HomePage"
+import ConceptPage from "./components/ConceptPage"
 
 
 function App() {
   return (
-    <>
-    <div className={styles.container}></div>
+    <div className={styles.container}>
       <Sidebar/>
-    </>
+      <Router>
+        <Routes> 
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:id" element={<ConceptPage />} />
+        </Routes>
+      </Router>
+
+    </div>
   )
 }
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Sidebar.module.scss'
 const Sidebar = () => {
   return (
     <div className={styles.container}>
-        
+        <h1>Sidebar</h1>
     </div>
   )
 }

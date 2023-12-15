9b9a408beab9aa105308cf02acf25521f541c95c
alexnyebuchan <alexnyebuchan@gmail.com>
2023-12-15 15:53:43 +0000

    installed router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styles from './styles/App.module.scss'
 import Sidebar from "./components/Sidebar"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,9 @@ import styles from '../styles/Sidebar.module.scss'
 
 const Sidebar = () => {
   return (
-    <div className={styles.container}>Sidebar</div>
+    <div className={styles.container}>
+        
+    </div>
   )
 }
 

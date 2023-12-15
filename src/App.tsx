d68ac37e1a6cf7ae7f0a9e326f3eb136369ca2d0
alexnyebuchan <alexnyebuchan@gmail.com>
2023-12-15 12:36:08 +0000

    created app and cleanup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './styles/App.module.scss'
+import Sidebar from "./components/Sidebar"
+
+
+function App() {
+  return (
+    <>
+    <div className={styles.container}></div>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Sidebar.module.scss'
+
+const Sidebar = () => {
+  return (
+    <div className={styles.container}>Sidebar</div>
+  )
+}
+
+export default Sidebar

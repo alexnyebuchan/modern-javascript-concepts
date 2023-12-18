b0f6e3fa0e2bc00b7f029f9b5cde6937e0eb7be1
alexnyebuchan <alexnyebuchan@gmail.com>
2023-12-18 23:02:17 +0000

    subs on side menu underway

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
         <Routes> 
           <Route path="/" element={<HomePage />} />
           <Route path="/:id" element={<ConceptPage />} />
+          <Route path="/:id/:id" element={<ConceptPage />} />
         </Routes>
         </Router>
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,16 +5,36 @@ import data from '../../data/category.json'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
 
 
 const Sidebar = () => {
-    console.log(data)
+
+const [hoveredLink, setHoveredLink] = useState(null)
+
+   const handleHover = (title) => {
+    setHoveredLink(title)
+   } 
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} >
         <h1>Modern JavaScript Concepts</h1>
         {data.map((cat) => (
-            <Link key={cat.title} to={`/${cat.title}`}><span>{cat.title}</span><FontAwesomeIcon icon={faChevronRight} /></Link>
+            <div key={cat.title} >
+                <Link to={`/${cat.title}`} 
+                onMouseEnter={() => handleHover(cat.title)} 
+               >
+                    <span>{cat.title}</span><FontAwesomeIcon icon={faChevronRight} /></Link>
+                {hoveredLink === cat.title && (
+                    <ul className={styles.subMenu} onMouseLeave={() => handleHover(null)}>
+                    {cat.subs.map((sub, index) => (
+                        <li key={index}>
+                            <Link to={`/${cat.title}/${sub}`}>{sub}</Link>
+                        </li>
+                    ))}
+                </ul>
+                )}
+            </div>
         ))}
     </div>
   )

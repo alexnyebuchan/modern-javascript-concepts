2291c07466ddc5fb5cf3838105a4f96126f7611d
alexnyebuchan <alexnyebuchan@gmail.com>
2024-01-04 23:21:35 +0000

    Added methods

--- src/components/SubCategoryPage.tsx
+++ src/components/SubCategoryPage.tsx
@@ -3,13 +3,13 @@ import { useParams } from 'react-router-dom';
 import categoryData from '../../data/category.json'
 import conceptData from '../../data/concepts.json'
 import { useEffect, useState } from 'react';
+import ReactEmbedGist from 'react-embed-gist';
+
 
 
 const SubCategoryPage = () => {
     const {categoryId, subId} = useParams()
     const [concept, setConcept] = useState({})
-
-
 
     useEffect(() => {
         findConcept(subId)
@@ -21,11 +21,21 @@ const SubCategoryPage = () => {
         setConcept(foundConcept)
     }
 
+    const body = {__html:concept.content};
 
   return (
     <div className={styles.container}>
         <h3>{subId}</h3>
-        <p>{concept.content}</p>
+        <div className={styles.body} dangerouslySetInnerHTML={body}></div>
+        {concept.gist && (
+            <ReactEmbedGist
+            gist={concept.gist}
+            contentClass={styles.gistContent}
+            titleClass={styles.gistTitle}
+            errorClass="gist__error"
+            />
+        )}
+        <p>{categoryId} {subId}</p>
     </div>
   )
 }
